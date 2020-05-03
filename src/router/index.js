ffaf78d2edd50f3236bc21cb5794207bcc7badc7
Maxi W <maxi.wolleb@gmail.com>
2020-05-03 12:48:37 +0200

    added error page +added view for error page +added entry in router

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,8 @@ import VueRouter from "vue-router"
 import VacationOverview from "@/views/VacationOverview.vue"
 import VacationShow from "@/views/VacationShow.vue"
 import VacationCreate from "@/views/VacationCreate.vue"
+import error from "@/views/error.vue"
 Vue.use(VueRouter)
-
-Vue.use(VueRouter);
 
 const routes = [
   {
@@ -25,20 +24,16 @@ const routes = [
     props: true
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "*",
+    name: "error",
+    component: error
   }
-];
+]
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
